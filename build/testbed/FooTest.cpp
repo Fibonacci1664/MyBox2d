9bc7041f771e9e627a76ff4a996daa0b15869858
Fibonacci <dave.green1664@yahoo.co.uk>
2021-06-11 10:16:01 +0100

    Created a body with shape and fixture for mass in my test case.

--- build/testbed/FooTest.cpp
+++ build/testbed/FooTest.cpp
@@ -6,7 +6,37 @@
 class FooTest : public Test
 {
 public:
-	FooTest() {}
+	FooTest()
+	{
+		createBody();
+	}
+
+	void createBody()
+	{
+		// Create body def.
+		b2BodyDef myBodyDef;
+		myBodyDef.type = b2_dynamicBody;	// This will be a dynamic body.
+		myBodyDef.position.Set(0, 20);		// Set a starting position.
+		myBodyDef.angle = 0;				// Set a starting angle.
+
+		// Create body.
+		b2Body* dynamicBody = m_world->CreateBody(&myBodyDef);
+
+		// Define body shape.
+		b2PolygonShape boxShape;
+		boxShape.SetAsBox(1, 1);			// Area = 1 * 1 = 1m^2
+
+		// Define body fixtures.
+		// http://www.iforce2d.net/b2dtut/bodies
+		// Mass = (Area of Fixture * Density of Fixture)
+		// Mass = 1 * 1 = 1kg
+		b2FixtureDef boxFixDef;
+		boxFixDef.shape = &boxShape;
+		boxFixDef.density = 1;
+
+		// Create body fixtures.
+		dynamicBody->CreateFixture(&boxFixDef);
+	}
 
 	void Step(Settings& settings)
 	{
