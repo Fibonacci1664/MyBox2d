92c621a381f57e02dcc260eec71436f893e9fd01
Fibonacci <dave.green1664@yahoo.co.uk>
2021-06-11 12:13:15 +0100

    Set some body properties.

--- build/testbed/FooTest.cpp
+++ build/testbed/FooTest.cpp
@@ -3,30 +3,37 @@
 
 #include "test.h"
 
+#define DEGTORAD 0.0174532925199432957f
+#define RADTODEG 57.295779513082320876f
+
 class FooTest : public Test
 {
 public:
+	b2Body* dynamicBody;
+
 	FooTest()
 	{
 		createBody();
+		setTransform();
+		setVelocities();
 	}
 
 	void createBody()
 	{
-		// Create body def.
+		// Create body def
 		b2BodyDef myBodyDef;
 		myBodyDef.type = b2_dynamicBody;	// This will be a dynamic body.
 		myBodyDef.position.Set(0, 20);		// Set a starting position.
 		myBodyDef.angle = 0;				// Set a starting angle.
 
-		// Create body.
-		b2Body* dynamicBody = m_world->CreateBody(&myBodyDef);
+		// Create body
+		dynamicBody = m_world->CreateBody(&myBodyDef);
 
-		// Define body shape.
+		// Define body shape
 		b2PolygonShape boxShape;
 		boxShape.SetAsBox(1, 1);			// Area = 1 * 1 = 1m^2
 
-		// Define body fixtures.
+		// Define body fixtures
 		// http://www.iforce2d.net/b2dtut/bodies
 		// Mass = (Area of Fixture * Density of Fixture)
 		// Mass = 1 * 1 = 1kg
@@ -34,8 +41,23 @@ public:
 		boxFixDef.shape = &boxShape;
 		boxFixDef.density = 1;
 
-		// Create body fixtures.
+		// Create body fixtures
 		dynamicBody->CreateFixture(&boxFixDef);
+	}
+
+	void setTransform()
+	{
+		// Using rads
+		//dynamicBody->SetTransform(b2Vec2(10, 20), 1);
+
+		// Using degs
+		dynamicBody->SetTransform(b2Vec2(10, 20), 45 * DEGTORAD);	// 45 deg counter clockwise
+	}
+
+	void setVelocities()
+	{
+		dynamicBody->SetLinearVelocity(b2Vec2(-5, 5));		// Moving up and left 5 units/s
+		dynamicBody->SetAngularVelocity(-90 * DEGTORAD);	// 90 deg per sec clockwise
 	}
 
 	void Step(Settings& settings)
